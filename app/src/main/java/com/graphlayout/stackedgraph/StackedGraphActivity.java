7fc4a34e93bc667621267c34f665fb8ea269e0b0
Keerthini K <keerthini.developer@gmail.com>
2016-07-14 14:25:16 +0530

    textview for graph generated

--- app/src/main/java/com/graphlayout/stackedgraph/StackedGraphActivity.java
+++ app/src/main/java/com/graphlayout/stackedgraph/StackedGraphActivity.java
@@ -1,17 +1,41 @@
 package com.graphlayout.stackedgraph;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+import com.graphlayout.stackedgraph.graphfactory.StackGraphGenerator;
+
+import java.util.ArrayList;
 
 
 public class StackedGraphActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_stacked_graph);
+    Context context;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        context = getApplicationContext();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base_view);
+
+        RelativeLayout relativeLayout;
+        relativeLayout = (RelativeLayout) findViewById(R.id.mainLayout);
+
+        StackGraphModel stackGraphModel = new StackGraphModel();
+        ArrayList xUpSeries = new ArrayList();
+        xUpSeries.add(12);
+        xUpSeries.add(24);
+        xUpSeries.add(36);
+
+        ArrayList xDownSeries = new ArrayList();
+        xDownSeries.add(11);
+        xDownSeries.add(22);
+        xDownSeries.add(32);
+
+        StackGraphGenerator stackgen = new StackGraphGenerator(context);
+        relativeLayout.addView(stackgen.generateStackedGraph(stackGraphModel, xUpSeries, xDownSeries));
 
     }
 }
--- app/src/main/java/com/graphlayout/stackedgraph/graphfactory/StackGraphGenerator.java
+++ app/src/main/java/com/graphlayout/stackedgraph/graphfactory/StackGraphGenerator.java
@@ -1,11 +1,15 @@
 package com.graphlayout.stackedgraph.graphfactory;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.graphlayout.stackedgraph.R;
 import com.graphlayout.stackedgraph.StackGraphModel;
@@ -18,17 +22,76 @@ import java.util.ArrayList;
 
 public class StackGraphGenerator extends View {
     Context context;
+    double highest = 1;
+    double highestDown = 1;
+    LinearLayout linearUpLayout,linearDownLayout;
+    ArrayList<Integer> xUpSeries;
+    ArrayList<Integer> xDownSeries;
 
     public StackGraphGenerator(Context context) {
         super(context);
         this.context = context;
     }
 
-    public void generateStackedGraph(StackGraphModel stackModel, ArrayList xUpSeries, ArrayList xDownSeries) {
-        RelativeLayout mainBaseLayout;
+    public View generateStackedGraph(StackGraphModel stackModel, ArrayList<Integer> xUpSeries, ArrayList<Integer> xDownSeries) {
+
         LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        mainBaseLayout = (RelativeLayout) findViewById(R.id.mainLayout);
-        View dynamicGraph = layoutInflater.inflate(R.layout.activity_stacked_graph, null);
-        mainBaseLayout.addView(dynamicGraph);
+        View dynamicGraph = layoutInflater.inflate(R.layout.activity_stacked_linear_layout, null);
+        linearUpLayout = (LinearLayout) dynamicGraph.findViewById(R.id.xUpLinearLayout);
+        linearDownLayout=(LinearLayout)dynamicGraph.findViewById(R.id.xDownLinearLayout);
+        this.xUpSeries = xUpSeries;
+        this.xDownSeries = xDownSeries;
+        generateGraph();
+        return dynamicGraph;
+    }
+
+    private void generateGraph() {
+
+        for (Integer highestXup : xUpSeries) {
+            if (highestXup > highest) {
+                highest = highestXup;
+            }
+        }
+
+        for (Integer highestDownn : xUpSeries) {
+            if (highestDownn > highestDown) {
+                highestDown = highestDownn;
+            }
+        }
+
+
+        LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        llp.setMargins(0, 0, 10, 0);
+        int xUpSize = xUpSeries.size();
+
+        for (int i = 0; i < xUpSize; i++) {
+            TextView xupTextView = new TextView(context);
+            xupTextView.setLayoutParams(llp);
+            xupTextView.setText(String.valueOf(xUpSeries.get(i)));
+            xupTextView.setTextColor(Color.BLUE);
+            xupTextView.setGravity(Gravity.BOTTOM);
+            double currentHeight = (xUpSeries.get(i) / highest) * 200;
+            xupTextView.setHeight((int) currentHeight);
+            xupTextView.setWidth(30);
+            xupTextView.setBackgroundColor(0xff66ff66);
+            linearUpLayout.addView(xupTextView);
+        }
+
+            int xDownSize = xDownSeries.size();
+
+            for (int j = 0; j < xDownSize; j++) {
+                TextView xdownTextView = new TextView(context);
+                xdownTextView.setLayoutParams(llp);
+                xdownTextView.setText(String.valueOf(xDownSeries.get(j)));
+                xdownTextView.setTextColor(Color.BLUE);
+                xdownTextView.setGravity(Gravity.TOP);
+                double downHeight = (xDownSeries.get(j) / highest) * 200;
+                xdownTextView.setHeight((int) downHeight);
+                xdownTextView.setWidth(30);
+                xdownTextView.setBackgroundColor(0xff66ff66);
+                linearDownLayout.addView(xdownTextView);
+            }
+
     }
 }
